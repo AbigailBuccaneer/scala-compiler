572ac6f4e01a55250b3f71081a431c42b1c46035
Abigail Bunyan <AbigailBuccaneer@users.noreply.github.com>
2017-02-28 14:13:21 +0000

    Add scala -bootclasspath in runner

--- src/main/java/io/github/abigailbuccaneer/ScalaRunner.java
+++ src/main/java/io/github/abigailbuccaneer/ScalaRunner.java
@@ -4,9 +4,15 @@ import org.apache.ivy.core.report.ArtifactDownloadReport;
 import org.apache.ivy.util.DefaultMessageLogger;
 import org.apache.ivy.util.Message;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ScalaRunner {
     public static void main(String[] args) {
@@ -24,8 +30,24 @@ public class ScalaRunner {
                 System.exit(1);
             }
         }
+        List<String> actualArgs = new ArrayList<>(args.length + 2);
+        actualArgs.add("-bootclasspath");
+        StringBuilder bootClasspath = new StringBuilder();
+        String delimeter = "";
+        for (URL url : urls) {
+            try {
+                bootClasspath.append(delimeter).append(new File(url.toURI()).getCanonicalPath());
+                delimeter = File.pathSeparator;
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+        actualArgs.add(bootClasspath.toString());
+
+        actualArgs.addAll(Arrays.asList(args));
+
         try {
-            new MainClassLoader(urls).invokeMain("scala.tools.nsc.MainGenericRunner", args);
+            new MainClassLoader(urls).invokeMain("scala.tools.nsc.MainGenericRunner", actualArgs.toArray(new String[]{}));
         } catch (InvocationTargetException | IllegalAccessException e) {
             e.printStackTrace();
             System.exit(1);
